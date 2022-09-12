fe2e9c28692aa6c9b569d6a72ee02df6e3ea84a1
Xkaminari <chahbounmedyassine@gmail.com>
2022-09-12 21:11:58 +0100

    animation background menu deroulée

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
-function expendMenu () {
-    document.getElementById("nav-bar").style.height = "90vh";
+function expendMenu() {
+    document.getElementById("nav-bar").style.height= "90vh";
+    document.getElementById("back-ground-img-menu").style.top = "0px";
     document.getElementById("slogant").style.color = "#ffffff00";
-    document.getElementById("back-ground-img-menu").style.height = "100%";
-    // document.getElementById("back-ground-img-menu").style.width = "100%";
+    document.getElementById("back-ground-img-menu").style.height = "100vh";
 }
