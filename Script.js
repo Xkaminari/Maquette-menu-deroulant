8c46cfec295cff2182a9af7a95de10dd6296a30c
Xkaminari <chahbounmedyassine@gmail.com>
2022-09-12 20:04:05 +0100

    first commit

---
+++ Script.js
@@ -0,0 +1,6 @@
+function expendMenu () {
+    document.getElementById("nav-bar").style.height = "90vh";
+    document.getElementById("slogant").style.color = "#ffffff00";
+    document.getElementById("back-ground-img-menu").style.height = "100%";
+    // document.getElementById("back-ground-img-menu").style.width = "100%";
+}
